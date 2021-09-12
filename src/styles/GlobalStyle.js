10527d983c9ddd1e14721c55ded1010b77bea816
Fábio Alves <fabioalves.contato@gmail.com>
2021-09-12 15:50:47 -0100

    chore: add root colors

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -8,7 +8,10 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   :root {
-
+    --dark-text: hsl(229, 25%, 31%);
+    --score-text: hsl(229, 64%, 46%);
+    --header-outline: hsl(217, 16%, 45%);
+    --white: hsl(0, 0%, 100%);
   }
 
   body {
